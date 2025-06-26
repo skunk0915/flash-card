05cbb7a7aff0f5934354e7f7ed060af41f07aa41
mizow <mizoguchi@outward.jp>
2025-06-26 16:35:19 +0900

    configのダミーを編集

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,15 +1,15 @@
 <?php
 // データベース設定
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'flashcard_app');
-define('DB_USER', 'your_username');
-define('DB_PASS', 'your_password');
+define('DB_HOST', 'mysql80.mizy.sakura.ne.jp');
+define('DB_NAME', 'mizy_flash');
+define('DB_USER', 'mizy');
+define('DB_PASS', '8rjcp4ck');
 define('DB_CHARSET', 'utf8mb4');
 
 // アプリケーション設定
 define('APP_NAME', '単語帳アプリ');
 define('APP_VERSION', '1.0.0');
-define('APP_URL', 'https://your-domain.com');
+define('APP_URL', 'https://mizy.sakura.ne.jp/flash-card/');
 
 // ファイルアップロード設定
 define('UPLOAD_DIR', '../frontend/assets/');
@@ -18,9 +18,9 @@ define('ALLOWED_IMAGE_TYPES', ['image/jpeg', 'image/png', 'image/gif']);
 define('ALLOWED_AUDIO_TYPES', ['audio/mp3', 'audio/wav', 'audio/ogg']);
 
 // プッシュ通知設定
-define('PUSH_SERVICE_URL', 'https://your-render-app.onrender.com');
-define('VAPID_PUBLIC_KEY', 'your_vapid_public_key');
-define('VAPID_PRIVATE_KEY', 'your_vapid_private_key');
+define('PUSH_SERVICE_URL', 'https://flash-card-ymtw.onrender.com');
+define('VAPID_PUBLIC_KEY', 'BLLLszuffWZYsrtAgZVWK_37QUWqsBF6PYGyAA3A2k6yTs_4obOgdk20o48iS8WQQmtVJmSOp9FeZM1ICLRP0xI');
+define('VAPID_PRIVATE_KEY', '1fwmUR7842tPJyRVALu5l3nXO8Yinnc_WrtdzkIbFbQ');
 
 // セキュリティ設定
 define('JWT_SECRET', 'your_jwt_secret_key');
